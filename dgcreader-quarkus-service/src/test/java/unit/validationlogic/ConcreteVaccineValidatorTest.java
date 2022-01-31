136edef635419f86f93be307dea3620c848287f4
Alessandro Bagnoli <alessandro.bagnoli4@gmail.com>
2022-01-31 23:42:17 +0100

    Started testing, added zxing as dependency

--- dgcreader-quarkus-service/src/test/java/unit/validationlogic/ConcreteVaccineValidatorTest.java
+++ dgcreader-quarkus-service/src/test/java/unit/validationlogic/ConcreteVaccineValidatorTest.java
@@ -1,15 +1,29 @@
 package unit.validationlogic;
 
+import static com.bagnoli.verificac19.dto.GPValidResponse.CertificateStatus.NOT_VALID_YET;
+import static com.bagnoli.verificac19.dto.GPValidResponse.CertificateStatus.VALID;
+import static java.util.Collections.singletonList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.bagnoli.verificac19.dto.GPValidResponse;
+import com.bagnoli.verificac19.dto.ValidationScanMode;
 import com.bagnoli.verificac19.service.downloaders.SettingsRetriever;
 import com.bagnoli.verificac19.service.validationlogic.ConcreteVaccineValidator;
 
 import se.digg.dgc.payload.v1.DigitalCovidCertificate;
+import se.digg.dgc.payload.v1.PersonName;
+import se.digg.dgc.payload.v1.VaccinationEntry;
 
 @ExtendWith(MockitoExtension.class)
 class ConcreteVaccineValidatorTest {
@@ -20,18 +34,90 @@ class ConcreteVaccineValidatorTest {
     private static final String VACCINE_END_DAY_COMPLETE = "vaccine_end_day_complete";
     private static final String JOHNSON = "EU/1/20/1525";
 
-    @Mock SettingsRetriever settingsRetriever;
-
-    @InjectMocks ConcreteVaccineValidator underTest;
+    private @Mock SettingsRetriever settingsRetriever;
+    private @InjectMocks ConcreteVaccineValidator underTest;
 
     @Test
-    void name() {
-//        // given
-//        DigitalCovidCertificate dgc = DigitalCovidCertificate.fromJsonString()
-//        
-//        // when
-//        underTest.calculateValidity();
-//        
-//        // then
+    void notValidYet_when_dateInFuture() {
+        // given
+        DigitalCovidCertificate dgc = (DigitalCovidCertificate) new DigitalCovidCertificate()
+            .withDob(LocalDate.of(1900, 1, 1))
+            .withNam(
+                new PersonName()
+                    .withFn("Mario")
+                    .withFnt("Mario")
+                    .withGn("Rossi")
+                    .withGnt("Rossi")
+            )
+            .withV(singletonList(
+                    new VaccinationEntry()
+                        .withCo("")
+                        .withCi("")
+                        .withDn(1)
+                        .withDt(LocalDate.of(2022, 2, 24))
+                        .withIs("")
+                        .withMa("")
+                        .withMp("EU/1/20/1525")
+                        .withSd(2)
+                        .withTg("")
+                        .withVp("")
+                )
+            );
+        given(settingsRetriever.getSettingValue(eq(VACCINE_END_DAY_COMPLETE), any())).willReturn(
+            15);
+        given(settingsRetriever.getSettingValue(eq(VACCINE_START_DAY_NOT_COMPLETE),
+            any())).willReturn(0);
+        given(
+            settingsRetriever.getSettingValue(eq(VACCINE_END_DAY_NOT_COMPLETE), any())).willReturn(
+            60);
+
+        // when
+        GPValidResponse response =
+            underTest.calculateValidity(dgc, ValidationScanMode.NORMAL_DGP);
+
+        // then
+        assertEquals(NOT_VALID_YET, response.getCertificateStatus());
+    }
+
+    @Test
+    void valid_when_firstDoseOfJohnsonNormalVerification() {
+        // given
+        DigitalCovidCertificate dgc = (DigitalCovidCertificate) new DigitalCovidCertificate()
+            .withDob(LocalDate.of(1900, 1, 1))
+            .withNam(
+                new PersonName()
+                    .withFn("Mario")
+                    .withFnt("Mario")
+                    .withGn("Rossi")
+                    .withGnt("Rossi")
+            )
+            .withV(singletonList(
+                    new VaccinationEntry()
+                        .withCo("")
+                        .withCi("")
+                        .withDn(1)
+                        .withDt(LocalDate.of(2022, 1, 10))
+                        .withIs("")
+                        .withMa("")
+                        .withMp("EU/1/20/1525")
+                        .withSd(2)
+                        .withTg("")
+                        .withVp("")
+                )
+            );
+        given(settingsRetriever.getSettingValue(eq(VACCINE_END_DAY_COMPLETE), any())).willReturn(
+            15);
+        given(settingsRetriever.getSettingValue(eq(VACCINE_START_DAY_NOT_COMPLETE),
+            any())).willReturn(0);
+        given(
+            settingsRetriever.getSettingValue(eq(VACCINE_END_DAY_NOT_COMPLETE), any())).willReturn(
+            60);
+
+        // when
+        GPValidResponse response =
+            underTest.calculateValidity(dgc, ValidationScanMode.NORMAL_DGP);
+
+        // then
+        assertEquals(VALID, response.getCertificateStatus());
     }
 }
